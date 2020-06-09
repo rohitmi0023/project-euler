290b3bd1f2a43e3a4ca5040164cd8e92b94296bf
rohit <you@example.com>
2020-06-09 11:58:41 +0530

    #3 added another efficient method

--- highestPrimeFactor/003.py
+++ highestPrimeFactor/003.py
@@ -1,13 +1,36 @@
-# Brute force solution 
+# Brute force solution
+import time
+import math
+start = time.time()
 def largest_prime_factor(num):
-    if num < 2:
-        print('Number should be greater than or equal to 2')
-        return
     highest_prime = 2
     for i in range(2, num + 1):
         while num % i == 0:
+            # print(num, 'num')
             highest_prime = i
-            num /= i 
+            num /= i
+            # print(num, 'num')
     return highest_prime
 
-print(largest_prime_factor(5))
+print(largest_prime_factor(144544))
+print(" %s seconds " % (time.time() - start))
+# Takes 0.0107049942017 seconds
+
+# 2
+# import time
+# import math
+start = time.time()
+def largest_prime_factor(num):
+    div = 2
+    while div <= math.floor(math.sqrt(num)):
+        if div > num:
+            return int(num)
+        if num % div == 0:
+            num /= div
+        else:
+            div += 1    
+    return int(num)
+    
+print(largest_prime_factor(144544))
+print(" %s seconds " % (time.time() - start))
+# Takes 2.19345092773e-05 seconds
