c74e7c40f07031c88825bed95eae7cec252c7045
rohit <you@example.com>
2020-06-10 11:16:04 +0530

    #4 added hackerrank solution

--- palindromeProduct/004.py
+++ palindromeProduct/004.py
@@ -1,4 +1,5 @@
-def checkPalindrome(number):
+# 1
+def check_palindrome(number):
     res = list(map(int, str(number)))
     length = len(res)
     count = 0
@@ -13,15 +14,35 @@ def checkPalindrome(number):
         return False
 
 
-def largestPalindromeProduct(num):
+def largest_palindrome_product(num):
     lower_limit = pow(10, num - 1)
     upper_limit = pow(10, num) - 1
     highest_palindrome = 0
     for i in range(upper_limit, lower_limit, -1):
         for j in range(upper_limit, lower_limit, -1):
-            if checkPalindrome(i * j):
+            if check_palindrome(i * j):
                 if i * j > highest_palindrome:
                     highest_palindrome = i * j
     return highest_palindrome
 
-print(largestPalindromeProduct(2))
+print(largest_palindrome_product(2))
+
+# 2 Hackerrank solution
+palindromelist = []
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        a = i * j
+        if str(a) == str(a)[::-1] and a not in palindromelist:
+            palindromelist.append(a)
+palindromelist.sort()
+length = len(palindromelist)
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for _ in range(n):
+        a = int(input())
+        for i in range(length - 1, -1, -1):
+            if palindromelist[i] < a:
+                print(palindromelist[i])
+                break
